6d8f165be15cf4aec7107b7a6288eaf768ae500e
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2019-09-27 17:50:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import flask
 from flask import request
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-from urllib.parse import urlparse
-from email.parser import BytesParser
 
 @app.route('/upload', methods=['POST'])
 def upload_img():
