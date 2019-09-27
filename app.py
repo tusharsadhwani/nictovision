40de20d7aec919bdf12a8d84cba6b6327b0a6637
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2019-09-27 18:27:22 +0530

    added image loading

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request
+# import draw_inference
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,4 +10,7 @@ def upload_img():
     img = request.files.get('upload_img', '')
     with open('uploaded_img.png', 'wb') as f:
         f.write(img.read())
+    
+    # new_path = draw_inference.infrence('uploaded_img', 'ios')
+    # print(new_path)
     return "ok"
