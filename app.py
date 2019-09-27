2722241fe7607795a8f9219181ed129e84a53d26
programmer290399 <programmer290399@gmail.com>
2019-09-27 15:15:52 +0530

    fixed dng

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import flask
 from flask import request
+from flask_cors import CORS
 import draw_inference
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['GET'])
+def homepage():
+    with open('index.html') as f:
+        return f.read()
+
+@app.route('/', methods=['POST'])
 def upload_img():
-    img = request.files.get('upload_img', '')
+    print(request.files)
+    img = request.files.get('upload_img', None)
     with open('uploaded_img.dng', 'wb') as f:
         f.write(img.read())
     
--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-function read_file(file) {
-  var input = file.target
+"use strict"
 
-  var reader = new FileReader()
-  reader.onload = function(){
-    let dataURL = reader.result
-    let uploaded_img = document.getElementById('uploaded_img')
-    uploaded_img.src = dataURL
-  }
-  img_data = reader.readAsDataURL(input.files[0])
-} 
